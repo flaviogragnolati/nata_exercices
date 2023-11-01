be5071ce589cf6fa7f8fb4106705f342c0de7510
flavio <flavio.gragnolati@gmail.com>
2023-11-01 20:24:36 -0300

    init

---
+++ Ejercicios/ejercicios1.js
@@ -0,0 +1,34 @@
+// 1. Crear una funcion que permita sumar 2 numeros
+
+// 2. Crear una funcion que permita sumar 2 numeros y que sanitize los inputs
+// ej. inputs: (1, 'hola'). En caso de tener un tipo que no sea numerico arrojar error
+
+// 3. Crear una funcion que permita sumar 2 numeros, sanitize los inputs y que intente parsear a numerico si es posible
+// ej. inpus: (1, '20'->'20' es parseable a 20. El resultado esperado es 21, no 120)
+
+// 4. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// 5. Devuelve un saludo en tres diferentes lenguajes:
+// Si "idioma" es "aleman", devuelve "Guten Tag!"
+// Si "idioma" es "mandarin", devuelve "Ni Hao!"
+// Si "idioma" es "ingles", devuelve "Hello!"
+// Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+
+// 6. Si "numero" es divisible entre 3, devuelve "fizz"
+// Si "numero" es divisible entre 5, devuelve "buzz"
+// Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+// De lo contrario, devuelve el numero.
+
+// 7. Funcion que obtenga el factorial de un numero pasado como argumento
+// ej. argumento: 5 -> 5! -> 120 es el resultado esperado
+
+// 8. Funcion que compare 2 inputs cualesquiera y devuelva un booleano si son exactamente iguales o no
+
+// 9. Funcion que recibe como input una cantidad `n` de argumentos y debe devolver un array de esos argumentos
+// ej. argumentos (1,2,3,4,'pirulo', true) -> [1,2,3,4,'pirulo', true]
+// la funcion tiene que poder recibir 0 o mas argumentos. En caso de no recibir argumentos devolver array vacio
+
+// 10. Funcion que al pasarle los argumentos de una ecuacion cuadratica obtenga las raices por metodo bhaskara
+// los argumentos son en la forma a,b,c
+// donde `a` corresponde al termino x^2; `b` corresponde al termino x; `c` corresponde al termino independiente
+// ej. argumentos (1,2,1) -> x1 = -1; x2 = -1
