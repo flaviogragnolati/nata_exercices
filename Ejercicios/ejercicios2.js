067e7d97a216e4819f6b3e6179eed9c88cd298e3
flavio <flavio.gragnolati@gmail.com>
2023-11-02 18:29:24 -0300

    ejercicios 2

---
+++ Ejercicios/ejercicios2.js
@@ -0,0 +1,27 @@
+// 1. Dado un array de numeros reales obtener la suma de los mismos
+// utilizando las siguientes funciones:
+// a. un for loop tradicional
+// b. un while loop
+// c. un loop con forEach (funcion de array)
+// d. la funcion `reduce` de los arrays
+
+// 2. Dado un array de strings, obtener el string mas largo de todos
+
+// 3. Dado un objecto con la forma { nombre: '...', edad: '...', ... } obtener un array de
+// strings con todas las propiedades del objeto.
+
+// 4. Dado el mismo objecto de arriba, obtener una tupla del tipo [key,value] de todas las propiedades del objeto
+
+// 5. Dado un array de numeros reales, obtener un array de strings con todos los numeros redondeados a 2 decimales
+
+// 6. Dado un array de strings, devolver un array de strings con todos los strings en mayusculas
+
+// 7. Dado un array de string, devolver un array con [longitud de string, string] del string mas largo
+
+// 8. Dado un array de numeros y strings, devolver un objeto con la sig. forma:
+// {numeros: [...], strings: [...], sumaNumeros: la suma de todos los numeros, laPalabraMasLarga: '...'}
+
+// 9. Dado un array de strings devolver la palabra que tenga la mayor ocurrencia de vocales
+
+// 10. Dado un array de strings y un string, devolver un array con todos los strings que contengan el string pasado como argumento.
+// ej. ['hola', 'chau', 'hola mundo', 'hola gente'], 'hola' -> ['hola', 'hola mundo', 'hola gente']
